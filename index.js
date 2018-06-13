3e3a1375ad88580fa19ffc2c210bebf7835b60b4
Ivan S Glazunov <ivansglazunov@gmail.com>
2018-06-13 23:33:58 +0000

    multi depth get

--- index.js
+++ index.js
@@ -3,32 +3,38 @@ var tb = require('travis-benchmark');
 var _ = require('lodash');
 var nestedProperty = require('nested-property');
 var objectPath = require("object-path");
+var async = require('async');
 
 var object = {"O73RO":{"ZWHKS":{"X3OPE":{"6F4C8":{"53GNL":{"YORMF":{"2CXEF":{"AIPED":{"IDG0W":{"BUOW1":{"TMPJI":"7A7U2"}}}}}}}}}}};
 var maxPath = [];
 for (var pointer = object; typeof pointer == 'object'; pointer = pointer[_.keys(pointer)[0]])
   maxPath.push(_.keys(pointer)[0]);
 
-var suite = new Benchmark.Suite(`get depth 1 by [object]`);
-
-suite.add('js (without checking)', function() {
-  object['O73RO'];
-});
-suite.add('lodash@4.17.10 get [string]', function() {
-  _.get(object, 'O73RO');
-});
-suite.add('lodash@4.17.10 get [array]', function() {
-  _.get(object, ['O73RO']);
-});
-suite.add('nested-property@0.0.7 get', function() {
-  nestedProperty.get(object, 'O73RO');
-});
-suite.add('object-path@0.11.4 get [string]', function() {
-  objectPath.get(object, 'O73RO');
-});
-suite.add('object-path@0.11.4 get [array]', function() {
-  objectPath.get(object, ['O73RO']);
-});
-
-tb.wrapSuite(suite);
-suite.run({ async: true });
+async.timesSeries(
+  maxPath.length,
+  function(t, next) {
+    var suite = new Benchmark.Suite(`get depth ${t+1} by [object]`);
+    
+    var path = maxPath.slice(0, t + 1);
+    
+    eval(`suite.add('js (without checking)', function() { object${_.map(path, p => `['${p}']`)}; });`);
+    suite.add('lodash@4.17.10 get [string]', function() {
+      _.get(object, path.join('.'));
+    });
+    suite.add('lodash@4.17.10 get [array]', function() {
+      _.get(object, path);
+    });
+    suite.add('nested-property@0.0.7 get', function() {
+      nestedProperty.get(object, path.join('.'));
+    });
+    suite.add('object-path@0.11.4 get [string]', function() {
+      objectPath.get(object, path.join('.'));
+    });
+    suite.add('object-path@0.11.4 get [array]', function() {
+      objectPath.get(object, path);
+    });
+    
+    tb.wrapSuite(suite, () => next());
+    suite.run({ async: true });
+  }
+);
