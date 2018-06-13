bffc7ebc16083974ce4f6689e1c237be5e4b1026
Ivan S Glazunov <ivansglazunov@gmail.com>
2018-06-13 23:49:58 +0000

    or work, my mistake

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var nestedProperty = require('nested-property');
 var objectPath = require("object-path");
 var async = require('async');
 var objectPathGet = require('object-path-get');
+var getObjectPath = require('get-object-path');
 var getValue = require('get-value');
 var objectResolvePath = require('object-resolve-path');
 var dotty = require('dotty');
@@ -21,7 +22,7 @@ async.timesSeries(
     var suite = new Benchmark.Suite(`get depth ${t+1} by [object]`);
     
     var arrPath = maxPath.slice(0, t + 1);
-    var strPath = strPath;
+    var strPath = strPath.join('.');
     
     eval(`suite.add('js (without checking)', function() { object${_.map(arrPath, p => `['${p}']`)}; });`);
     suite.add('lodash@4.17.10 get [string]', function() {
@@ -44,6 +45,9 @@ async.timesSeries(
     });
     suite.add('object-path-get@1.0.0 get [array]', function() {
       objectPathGet(object, arrPath);
+    });
+    suite.add('get-object-path@0.0.3 get [string]', function() {
+      getObjectPath(object, strPath);
     });
     suite.add('get-value@3.0.1 get [array]', function() {
       getValue(object, arrPath);
