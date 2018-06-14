b711976d806ff2abdedf4839561bffb981eff69e
Ivan S Glazunov <ivansglazunov@gmail.com>
2018-06-14 04:25:13 +0000

    valid path for object-resolve-path

--- index.js
+++ index.js
@@ -22,50 +22,51 @@ async.timesSeries(
     var suite = new Benchmark.Suite(`get depth ${t+1} by [object]`);
     
     var arrPath = maxPath.slice(0, t + 1);
-    var strPath = arrPath.join('.');
+    var strDotPath = arrPath.join('.');
+    var strSquarePath = _.map(arrPath, p => `["${p}"]`).join('');
     
     eval(`suite.add('js (without checking)', function() { object${_.map(arrPath, p => `['${p}']`)}; });`);
     suite.add('lodash@4.17.10 get [string]', function() {
-      _.get(object, strPath);
+      _.get(object, strDotPath);
     });
     suite.add('lodash@4.17.10 get [array]', function() {
       _.get(object, arrPath);
     });
     suite.add('nested-property@0.0.7 get', function() {
-      nestedProperty.get(object, strPath);
+      nestedProperty.get(object, strDotPath);
     });
     suite.add('object-path@0.11.4 get [string]', function() {
-      objectPath.get(object, strPath);
+      objectPath.get(object, strDotPath);
     });
     suite.add('object-path@0.11.4 get [array]', function() {
       objectPath.get(object, arrPath);
     });
     suite.add('object-path-get@1.0.0 get [string]', function() {
-      objectPathGet(object, strPath);
+      objectPathGet(object, strDotPath);
     });
     suite.add('object-path-get@1.0.0 get [array]', function() {
       objectPathGet(object, arrPath);
     });
     suite.add('get-object-path@0.0.3 get [string]', function() {
-      getObjectPath(object, strPath);
+      getObjectPath(object, strDotPath);
     });
     suite.add('get-value@3.0.1 get [array]', function() {
       getValue(object, arrPath);
     });
     suite.add('get-value@3.0.1 get [string]', function() {
-      getValue(object, strPath);
+      getValue(object, strDotPath);
     });
     suite.add('object-resolve-path@1.1.1 get [string]', function() {
-      objectResolvePath(object, strPath);
+      objectResolvePath(object, strSquarePath);
     });
     suite.add('dotty@0.1.0 get [array]', function() {
       dotty.get(object, arrPath);
     });
     suite.add('dotty@0.1.0 get [string]', function() {
-      dotty.get(object, strPath);
+      dotty.get(object, strDotPath);
     });
     suite.add('dot-access@1.0.0 get [string]', function() {
-      dotAccess.get(object, strPath);
+      dotAccess.get(object, strDotPath);
     });
     
     tb.wrapSuite(suite, () => next());
